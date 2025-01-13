23293a63ead44389074be9724171865e3bb235c5
Wolf Vollprecht <w.vollprecht@gmail.com>
2025-01-13 16:27:19 +0100

    .

--- verify.sh
+++ verify.sh
@@ -1,6 +1,6 @@
 # which sigstore
 
 sigstore verify github  \
-    --bundle wolfv-sigstore-test-attestation-2102070.sigstore.json \
+    --bundle wolfv-sigstore-test-attestation-2627564.sigstore.json \
     --cert-identity https://github.com/wolfv/sigstore-test/.github/workflows/action.yaml@refs/heads/main \
-    sha256:db468f8ac06acd10fef9c6bde46ce99abf75aa8578fe2b0948b3c08dbb798fba
+    sha256:96f5b18fb6fa4545bd78c6a5f58217b73bd16b45bf06b293ad50a95c510a1f38
